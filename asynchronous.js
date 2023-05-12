572bbf4fb77d6e94a1a269f649feebfdb7ec188c
wambui-nyambura <nyamburawambuui@gmail.com>
2023-05-12 19:00:54 +0300

    asynchronous functions

---
+++ asynchronous.js
@@ -0,0 +1,19 @@
+// Question1
+// Write an asynchronous function that accepts a message string and a delay time in milliseconds. 
+// The function should log the message to the console after the specified delay time.
+const study = ()=>{
+    console.log('JS is an easy language');
+}
+study()
+
+const revision = () =>{
+    console.log('JS requires a consistence in revision to ace in it.');
+}
+setTimeout(revision,3000)
+
+
+// Question2
+// You have an array of user IDs and a function getUserData(id) that returns a Promise with user data when given 
+// a user ID. Write an asynchronous function that fetches and logs the data for each user ID one by one, in sequence.
+
+
