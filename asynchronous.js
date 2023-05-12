9a65ec8c1403d97a256e8cc54d91727b340173fe
wambui-nyambura <nyamburawambuui@gmail.com>
2023-05-12 19:58:28 +0300

    changes

--- asynchronous.js
+++ asynchronous.js
@@ -1,15 +1,13 @@
 // Question1
 // Write an asynchronous function that accepts a message string and a delay time in milliseconds. 
 // The function should log the message to the console after the specified delay time.
-const study = ()=>{
-    console.log('JS is an easy language');
-}
-study()
-
-const revision = () =>{
-    console.log('JS requires a consistence in revision to ace in it.');
-}
-setTimeout(revision,3000)
+const logMessageWithDelay = async (message, delay) => {
+    await new Promise(resolve => setTimeout(resolve, delay));
+    console.log(message);
+  };
+  
+  logMessageWithDelay("Message with 2 second delay", 2000);
+  logMessageWithDelay("Message with no delay"); 
 
 
 // Question2
@@ -17,3 +15,38 @@ setTimeout(revision,3000)
 // a user ID. Write an asynchronous function that fetches and logs the data for each user ID one by one, in sequence.
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// You have an asynchronous function performTask() that returns a Promise. The Promise resolves if the task 
+// is successful and rejects if there's an error. Write a function that calls performTask() and logs a custom 
+// success message if the task is successful, and a custom error message if there's an error.
+const performTask = async () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("Task completed successfully");
+      }, 2000);
+    });
+  };
+  
+  const task = async () => {
+    try {
+      const result = await performTask();
+      console.log(`Success: ${result}`);
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  };
+  
+  task();
